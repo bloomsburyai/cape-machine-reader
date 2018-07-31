f37d5d050e7ce222c64490adfaf2359b2e6449ba
Luis Ulloa <timken@gmail.com>
2018-07-31 12:43:20 +0100

    New setup

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-import os
-import sys
-import subprocess
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
-
-# TODO is there a better way ? dependencies seem to always require the version
-# Calling only at the egg_info step gives us the wanted depth first behavior
-if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-    pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
-    if not os.path.isfile(pip3_executable):
-        pip3_executable = 'pip3'
-    subprocess.check_call([pip3_executable, 'install', '--upgrade', '-r', 'requirements.txt'])
 
 setup(
     name=NAME,
@@ -20,6 +9,11 @@ setup(
     author_email='contact@bloomsbury.ai',
     packages=PACKAGES,
     include_package_data=True,
+    install_requires=[
+        'pytest==3.6.4',
+        'numpy==1.15.0',
+        'pyinterval==1.2.0',
+    ],
     package_data={
         '': ['*.*'],
     },
